b85f8a2b3ad8bfede50721dfc5d0573cde382af5
samridhimathur <samridhimathur10@gmail.com>
2026-04-12 19:24:13 +0530

    enable properties for the app

--- opsbot/src/main/java/com/opsbot/OpsBotApplication.java
+++ opsbot/src/main/java/com/opsbot/OpsBotApplication.java
@@ -2,8 +2,12 @@ package com.opsbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import com.opsbot.config.CohereProperties;
+import com.opsbot.config.AnthropicProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({CohereProperties.class, AnthropicProperties.class})
 public class OpsBotApplication {
     public static void main(String[] args) {
         SpringApplication.run(OpsBotApplication.class, args);
